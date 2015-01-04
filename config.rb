7d8f05eeb8e738d6a510e86def9f0faff1f01ebd
Tony Cannistra <tony.cannistra@gmail.com>
2015-01-04 18:23:07 -0500

    transitioning to static site in client/

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "scss"
+css_dir = "client/stylesheets"
+sass_dir = "client/scss"
 images_dir = "public"
-javascripts_dir = "js"
+javascripts_dir = "client/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
